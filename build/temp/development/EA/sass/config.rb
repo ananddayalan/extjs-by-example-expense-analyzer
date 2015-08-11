086af43ec4ae68bb7cbda5f7e8395256ce8ba495
Anand Dayalan <ananddayalan@Anands-MacBook-Pro.local>
2015-08-11 16:07:23 +0400

    deleting build folder

--- build/temp/development/EA/sass/config.rb
+++
@@ -1,3 +0,0 @@
-require '../../../../../ext/classic/theme-base/sass/utils.rb'
-Compass.add_project_configuration('../../../../../sass/config.rb')
-cache_path = '/Users/ananddayalan/OneDrive/project/extjs/expense/build/.sass-cache'
--- build/temp/production/EA/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Users/ananddayalan/OneDrive/project/extjs/expense/app.js
